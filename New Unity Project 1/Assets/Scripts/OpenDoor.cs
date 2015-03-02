eafa2f0adff12ad38000a953e0e30fb87b68947d
Jared Pepper <jared737@msn.com>
2015-03-02 15:07:38 +0000

    Safe Scene
    The safe with an opening door.

---
+++ New Unity Project 1/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour 
+{
+
+    bool slide = false;
+    float WaitTime = 3;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+
+
+    void StopDoor()
+    {
+
+        slide = false;
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+
+        if (slide == true)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }
+
+       // Invoke ("StopDoor", 4); 
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 70, 80, 30), "Open Door"))
+        {
+            slide = true;
+            Invoke("StopDoor", 4); 
+
+        }
+    }
+
+
+
+}
+

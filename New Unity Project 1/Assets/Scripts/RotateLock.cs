909f954c7692a670690dc503ed798cabcf048055
dayo351 <dayooye351@hotmail.co.uk>
2015-03-05 12:42:07 +0000

    Updated Safe
    The safe buttons(Right, left and log) cannot be pressed whilst the dial
    is rotating.

--- New Unity Project 1/Assets/Scripts/RotateLock.cs
+++ New Unity Project 1/Assets/Scripts/RotateLock.cs
@@ -14,10 +14,19 @@ public class RotateLock : MonoBehaviour
     public bool codeNum2 = false;
     public bool codeNum3 = false;
     public GameObject dial;
+    bool guiOn = true;
 
     // Use this for initialization
     void Start()
     {
+
+    }
+
+    void ButtonReappear()
+    {
+        
+            guiOn = true;
+        
 
     }
 
@@ -27,11 +36,14 @@ public class RotateLock : MonoBehaviour
         if (rotateRight == true)
         {
             transform.Rotate(Vector3.up * Time.deltaTime * 24.5f);
+         
         }
 
         if (rotateLeft == true)
         {
             transform.Rotate(Vector3.down * Time.deltaTime * 24.5f);
+            
+
         }
 
 
@@ -131,73 +143,88 @@ public class RotateLock : MonoBehaviour
 
     void OnGUI()
     {
-        if (GUI.Button(new Rect(320, 630, 110, 30), "Rotate Right"))
+
+
+        if (guiOn == true)
         {
-            rotateRight = true;
-            rotateLeft = false;
-            Invoke("stopRotate", 1.445f);
 
-            codeHelper += 1;
-
-            if (codeHelper > 109)
+            if (GUI.Button(new Rect(320, 630, 110, 30), "Rotate Right"))
             {
-                codeHelper = 100;
+                rotateRight = true;
+                rotateLeft = false;
+
+                Invoke("stopRotate", 1.445f);
+                guiOn = false;
+
+                Invoke("ButtonReappear", 1.35f);
+
+                codeHelper += 1;
+
+                if (codeHelper > 109)
+                {
+                    codeHelper = 100;
+                }
+
             }
 
-        }
-
-        if (GUI.Button(new Rect(60, 630, 110, 30), "Rotate Left"))
-        {
-            rotateLeft = true;
-            rotateRight = false;
-            Invoke("stopRotate", 1.445f);
-
-            codeHelper -= 1;
-
-
-            if (codeHelper <= 90)
+            if (GUI.Button(new Rect(60, 630, 110, 30), "Rotate Left"))
             {
-                codeHelper = 100;
-            }
+                rotateLeft = true;
+                rotateRight = false;
+                Invoke("stopRotate", 1.445f);
 
+                guiOn = false;
+                Invoke("ButtonReappear", 1.35f);
+
+                codeHelper -= 1;
+
+
+                if (codeHelper <= 90)
+                {
+                    codeHelper = 100;
+                }
+
+            }
         }
 
         GUI.Label(new Rect(190, 500, 110, 30), "Code is: 10, 40, 70");
 
-            
-
-        if (GUI.Button(new Rect(190, 580, 110, 30), "Log Number"))
+        if (guiOn == true)
         {
-            if (code == 1)
+
+            if (GUI.Button(new Rect(190, 580, 110, 30), "Log Number"))
             {
-                codeNum1 = true;
-            }
-            if (code == 4)
-            {
-                codeNum2 = true;
-            }
-            if (code == 7)
-            {
-                codeNum3 = true;
-            }
+                if (code == 1)
+                {
+                    codeNum1 = true;
+                }
+                if (code == 4)
+                {
+                    codeNum2 = true;
+                }
+                if (code == 7)
+                {
+                    codeNum3 = true;
+                }
 
 
-            if (codeNum1 == true && codeNum2 == true && codeNum3 == true)
-            {
-                dial.GetComponentInParent<OpenDoor>().OpenTheDoor();
-                
-            }
+                if (codeNum1 == true && codeNum2 == true && codeNum3 == true)
+                {
+                    dial.GetComponentInParent<OpenDoor>().OpenTheDoor();
+
+                }
 
 
-            if (code == 0 || code == 2 || code == 3 || code == 5 || code == 6 || code == 8 || code == 9)
-            {
+                if (code == 0 || code == 2 || code == 3 || code == 5 || code == 6 || code == 8 || code == 9)
+                {
 
-                     codeNum1 = false;    
-                     codeNum2 = false;
-                     codeNum3 = false;
+                    codeNum1 = false;
+                    codeNum2 = false;
+                    codeNum3 = false;
 
-               
 
+
+                }
             }
         }
 

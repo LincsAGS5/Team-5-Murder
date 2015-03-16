2753df10f98cb2fdca45e1fb161ac363dbcf4c04
Jared Pepper <jared737@msn.com>
2015-03-16 13:24:24 +0000

    All Safe stuff in master.

---
+++ Unity/Assets/JaredsStuff/Scripts/Handle Script.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandleScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(Vector3.up * Time.deltaTime * 24.5f);
+
+
+    }
+}
---
+++ Unity/Assets/JaredsStuff/Scripts/OpenDoor.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour 
+{
+
+  public  bool SlideOpenTheDoor = false;
+	public AudioClip doorOpen;
+
+    float WaitTime = 3;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+
+
+    void StopDoor()
+    {
+
+        SlideOpenTheDoor = false;
+    }
+
+
+    public void OpenTheDoor()
+    {
+
+        SlideOpenTheDoor = true;
+        Invoke("StopDoor", 4); 
+		this.GetComponent<AudioSource>().Play();
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+
+        if (SlideOpenTheDoor == true)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+
+       // Invoke ("StopDoor", 4); 
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(175, 540, 140, 30), "Force the Door Open!"))
+        {
+
+            OpenTheDoor();
+        }
+    }
+
+
+
+}
+

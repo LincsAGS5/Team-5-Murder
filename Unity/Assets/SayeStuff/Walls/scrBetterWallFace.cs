b895cb0a5b306bf84d7a10b21e2ca2e54321bd67
Alexander Saye <alexandersaye@live.co.uk>
2015-03-22 22:15:19 +0000

    Wall Replacement & Texture Scale Fixing Script
    All walls replaced with cubes of quads, with unseen faces removed.
    Autodestructing script attached to each quad that automatically (lots of
    auto's here) fixes the scale of the quad's textures for a uniform
    appearance across walls.

---
+++ Unity/Assets/SayeStuff/Walls/scrBetterWallFace.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Helper component that automatically fixes texture scaling on the faces of my BetterWall objects.
+public class scrBetterWallFace : MonoBehaviour
+{
+	private static string[] textureNames = new string[] { "_MainTex", "_BumpMap" };
+
+	public Vector2 FixedScale = Vector2.one;
+
+	void Awake ()
+	{
+		// Change the material scale to account for the scale of the face it is assigned to.
+		foreach (string texture in textureNames)
+		{
+			// Check whether the material has the texture property.
+			if (renderer.material.HasProperty(texture))
+			{
+				// Modify the texture's scales.
+				Vector2 scale = renderer.material.GetTextureScale(texture);
+				scale.x = transform.lossyScale.x * scale.x / FixedScale.x;
+				scale.y =  transform.lossyScale.y * scale.y / FixedScale.y;
+				renderer.material.SetTextureScale(texture, scale);
+			}
+		}
+
+		// Destroy this component, it has no further function.
+		Destroy (this);
+	}
+}
